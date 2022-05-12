17c0fa925074da8dea6decbf8f1c8f729d2761a1
SatoShun <sato.shun2010@gmail.com>
2022-05-13 00:36:17 +0900

    W08_task2_Finish

--- W08/Task2_main.js
+++ W08/Task2_main.js
@@ -1,6 +1,6 @@
 d3.csv("https://ssatoshun.github.io/Information_Visualization/W08/W08_task2_data.csv")
     .then( data => {
-        data.forEach( d => { d.label = d.label; d.value = +d.value; d.color = d.color;});
+        data.forEach( d => { d.x = +d.x; d.y = +d.y; d.color = d.color;});
 
         var config = {
             parent: '#drawing_region',
@@ -9,13 +9,13 @@ d3.csv("https://ssatoshun.github.io/Information_Visualization/W08/W08_task2_data
             margin: {top:30, right:20, bottom:30, left:80}
         };
 
-        const bar_chart = new BarChart( config, data );
-        bar_chart.update();
+        const line_chart = new LineChart( config, data );
+        line_chart.update();
     })
     .catch( error => {
         console.log( error );
     });
-class BarChart {
+class LineChart {
 
       constructor( config, data ) {
           this.config = {
@@ -41,7 +41,7 @@ class BarChart {
           self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
           self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
   
-          self.xscale = d3.scaleBand()
+          self.xscale = d3.scaleLinear()
               .range( [0, self.inner_width] );
   
           self.yscale = d3.scaleLinear()
@@ -57,29 +57,36 @@ class BarChart {
           self.xaxis_group = self.chart.append('g')
               .attr('transform', `translate(0, ${self.inner_height})`);
 
-          self.yaxis_group = self.chart.append('g')
-              .attr('transform', `translate(-3,0)`);
+          self.yaxis_group = self.chart.append('g');
         
           self.svg.append("text")
-            .attr("x",self.inner_width/2)
+            .attr("x",self.inner_width/2 - self.config.margin.right*2)
             .attr("y",self.config.margin.top-7)
-            .attr("font-size","24px")
+            .attr("font-size","18px")
             .attr("fill","black")
             .attr("stroke","black")
             .attr("font-weight",20)
-            .attr("stroke-width",1.8)
-            .text("Bar Chart");
+            .attr("stroke-width",1.6)
+            .text("Line Chart with Dots and Area");
+
+          self.area = d3.area()
+            .x( d => self.xscale(d.x ))
+            .y1( d => self.yscale(d.y) )
+            .y0(self.inner_height);
+          self.line = d3.line()
+            .x( d => self.xscale(d.x ))
+            .y( d => self.yscale(d.y) );
       }
   
       update() {
           let self = this;
-          self.xscale.domain( self.data.map(d => d.label ))
-          .paddingInner(0.3);
-          //self.yscale.domain( self.data.map(d => d.label ))
-          //.paddingInner(0.3);
+          const xmin = d3.min(self.data, d => d.x);
+          const xmax = d3.max(self.data, d => d.x);
+          self.xscale.domain( [xmin,xmax]);
 
-          const xmax = d3.max( self.data, d => d.value );
-          self.yscale.domain( [0,xmax] );
+          const ymax = d3.max(self.data, d => d.y);
+
+          self.yscale.domain( [0,ymax] );
 
           self.render();
       }
@@ -87,31 +94,42 @@ class BarChart {
       render() {
           let self = this;
   
-          self.chart.selectAll("rect")
+          self.chart.append("path")
+              .attr("d", self.area(self.data))
+              .attr('fill', '#999')
+              .attr('stroke', 'black');
+
+          self.chart.append("path")
+              .attr("d", self.line(self.data))
+              .attr('stroke', 'blue')
+              .attr("stroke-width",2)
+              .attr("fill","none");
+              
+
+          self.chart.selectAll("circle")
               .data(self.data)
               .enter()
-              .append("rect")
-              .attr("x", d => self.xscale( d.label))
-              .attr("y", d => self.yscale( d.value) )
-              .attr("height", d => self.inner_height-self.yscale(d.value ))
-              .attr("width", self.xscale.bandwidth())
-              .attr("fill",function(d){return d.color;});
+              .append("circle")
+              .attr("cx", function(d){ return self.xscale(d.x); })
+              .attr("cy", function(d){ return self.yscale(d.y); })
+              .attr("r", function(d){ return 4; })
+              .style("fill","red")
 
 
               self.chart.selectAll("text")
                  .data(self.data)
                  .enter()
                  .append("text")
-                 .attr("fill","white")
-                 .text(function(d) {
-                    return d.value;
-                 })
-                 .attr("x", function(d, i) {
-                    return self.xscale( d.label) + self.xscale.bandwidth()/self.data.length;
-                 })
-                 .attr("y", function(d) {
-                    return self.yscale(d.value)+20;
-                 });
+                 //.attr("fill","white");
+                  .text(function(d) {
+                     return d.y;
+                  })
+                  .attr("x", function(d, i) {
+                     return self.xscale(d.x)+1;
+                  })
+                  .attr("y", function(d) {
+                     return self.yscale(d.y)+12;
+                  });
                    
           self.xaxis_group
               .call( self.xaxis )
@@ -119,8 +137,7 @@ class BarChart {
               .attr("x",self.inner_width/2)
               .attr("y",self.config.margin.bottom)
               .attr("font-size", "18px")
-              .text("Food Name")
-              .attr("dy",2)
+              .text("XLabel")
               .attr("fill","black")
               .attr("stroke","black")
               .attr("stroke-width",1);
@@ -131,7 +148,7 @@ class BarChart {
               .attr("x",-self.config.margin.right-self.inner_height/2)
               .attr("y",-self.config.margin.left/2)
               .attr("font-size", "18px")
-              .text("Value")
+              .text("YLabel")
               .attr("fill","black")
               .attr("stroke","black")
               .attr('transform', 'rotate(-90)')
